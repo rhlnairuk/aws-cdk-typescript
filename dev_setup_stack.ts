f403e40e15487cdd4f026bbfd7cb994f31af7dc6
Rahul Nair <rhlnair87@gmail.com>
2024-01-26 09:57:07 +0000

    Versioning typescript codebase

---
+++ dev_setup_stack.ts
@@ -0,0 +1,109 @@
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as rds from '@aws-cdk/aws-rds';
+import * as elbv2 from '@aws-cdk/aws-elasticloadbalancingv2';
+import * as autoscaling from '@aws-cdk/aws-autoscaling';
+import { Stack, Construct, StackProps } from '@aws-cdk/core';
+
+export class NetworkingStack extends Stack {
+  public readonly vpc: ec2.Vpc;
+
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const envType = this.node.tryGetContext('env_type');
+    const resourcePrefix = this.node.tryGetContext(envType).resourcePrefix;
+
+    this.vpc = new ec2.Vpc(this, `${resourcePrefix}VPC`, {
+      maxAzs: 3,
+      natGateways: 1,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'PublicSubnet',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: 'PrivateSubnetWithNat',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_NAT,
+        },
+        {
+          cidrMask: 24,
+          name: 'IsolatedSubnet',
+          subnetType: ec2.SubnetType.ISOLATED,
+        },
+      ],
+    });
+  }
+}
+export class DevStack extends Stack {
+  constructor(scope: Construct, id: string, vpc: ec2.Vpc, props?: StackProps) {
+    super(scope, id, props);
+
+    const webServerUserData = ec2.UserData.forLinux();
+    webServerUserData.addCommands(
+      "echo 'Configuring as web server'",
+      "# Add application server specific setup commands here"
+    );
+
+    const appServerUserData = ec2.UserData.forLinux();
+    appServerUserData.addCommands(
+      "echo 'Configuring as app server'",
+      "# Add application server specific setup commands here"
+    );
+
+    // Define the criteria for the AMI
+    const amiFilters = {
+      filters: {
+        'name': 'amzn2-ami-hvm-*-x86_64-gp2', // Example pattern
+      },
+      owners: ['amazon'], // Specify the owner account ID
+    };
+
+    const latestAmi = ec2.MachineImage.lookup(amiFilters);
+
+    const lbSg = new ec2.SecurityGroup(this, 'LoadBalancerSG', {
+      vpc,
+      allowAllOutbound: true,
+      description: 'Security group for load balancer'
+    });
+    lbSg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80));
+
+    const alb = new elbv2.ApplicationLoadBalancer(this, 'ALB', {
+      vpc,
+      internetFacing: true,
+      securityGroup: lbSg
+    });
+
+    const listener = alb.addListener('WebListener', { port: 80 });
+
+    const webServerAsg = new autoscaling.AutoScalingGroup(this, 'WebServersAsg', {
+      vpc,
+      vpcSubnets: { subnetType: ec2.SubnetType.PUBLIC },
+      userData: webServerUserData,
+      instanceType: new ec2.InstanceType('t3.micro'),
+      machineImage: latestAmi,
+      minCapacity: 1,
+      maxCapacity: 5
+    });
+
+    listener.addTargetGroups('Target', {
+      targetGroups: [webServerAsg]
+    });
+
+    webServerAsg.scaleOnCpuUtilization('CpuScaling', {
+      targetUtilizationPercent: 50
+    });
+
+    // Create an EC2 instance for the application server
+    new ec2.Instance(this, 'AppServers', {
+      userData: appServerUserData,
+      instanceType: new ec2.InstanceType('t3.micro'),
+      machineImage: ec2.MachineImage.latestAmazonLinux(),
+      vpc,
+      vpcSubnets: { subnetType: ec2.SubnetType.PRIVATE_WITH_NAT }
+    });
+
+    // Additional resources like RDS instance can be added here
+  }
+}
--- main.ts
+++ main.ts
@@ -1,75 +1,17 @@
+#!/usr/bin/env node
 import * as cdk from '@aws-cdk/core';
-import * as ec2 from '@aws-cdk/aws-ec2';
-import * as rds from '@aws-cdk/aws-rds';
-import * as elbv2 from '@aws-cdk/aws-elasticloadbalancingv2';
-
-class MyStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-
-    // Create a VPC
-    const vpc = new ec2.Vpc(this, 'MyVPC', {
-      maxAzs: 3 // Default is all AZs in the region
-    });
-
-    // Create an Application Load Balancer
-    const lb = new elbv2.ApplicationLoadBalancer(this, 'LB', {
-      vpc,
-      internetFacing: true
-    });
-
-    const listener = lb.addListener('Listener', {
-      port: 80,
-    });
-
-    // Create Security Groups for EC2 instances and RDS
-    const ec2SecurityGroup = new ec2.SecurityGroup(this, 'ec2SecurityGroup', {
-      vpc,
-      description: 'Security group for EC2 instances',
-      allowAllOutbound: true
-    });
-
-    const rdsSecurityGroup = new ec2.SecurityGroup(this, 'rdsSecurityGroup', {
-      vpc,
-      description: 'Security group for RDS instance',
-      allowAllOutbound: true
-    });
-
-    // Allow web traffic to EC2 instances
-    ec2SecurityGroup.addIngressRule(
-      ec2.Peer.anyIpv4(),
-      ec2.Port.tcp(80),
-      'Allow HTTP traffic from anywhere'
-    );
-
-    // EC2 instance for web/application server
-    const ec2Instance = new ec2.Instance(this, 'Instance', {
-      vpc,
-      instanceType: new ec2.InstanceType('t3.micro'),
-      machineImage: new ec2.AmazonLinuxImage(),
-      securityGroup: ec2SecurityGroup
-    });
-
-    // Add EC2 instance to the load balancer
-    listener.addTargets('Target', {
-      port: 80,
-      targets: [ec2Instance]
-    });
-
-    // Create an RDS instance
-    const dbInstance = new rds.DatabaseInstance(this, 'Instance', {
-      engine: rds.DatabaseInstanceEngine.mysql({
-        version: rds.MysqlEngineVersion.VER_8_0_19
-      }),
-      instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO),
-      vpc,
-      securityGroups: [rdsSecurityGroup]
-    });
-
-    // Output the DNS name of the load balancer
-    new cdk.CfnOutput(this, 'LoadBalancerDNS', { value: lb.loadBalancerDnsName });
-  }
-}
+import { NetworkingStack } from './dev_setup_stack';
+import { DevStack } from './dev_setup_stack';
 
 const app = new cdk.App();
-new MyStack(app, 'MyStack');
+
+const envDefault = {
+  region: 'us-east-1',
+  account: process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT
+};
+
+const networkStack = new NetworkingStack(app, 'NetworkingStack', { env: envDefault });
+const devStack = new DevStack(app, 'DevStack', networkStack.vpc, { env: envDefault });
+devStack.addDependency(networkStack);
+
+app.synth();
